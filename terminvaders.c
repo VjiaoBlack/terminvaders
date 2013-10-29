7d651567dd770ecdd0dcab8d8c634411d2c82837
Ben Kurtovic <ben.kurtovic@gmail.com>
2013-10-29 11:43:34 -0400

    Update.

--- terminvaders.c
+++ terminvaders.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-// not sure if we're doing delta time algorithm
 #include <unistd.h>
-
 #include "terminvaders.h"
 
-// #include <rest-of-files.h>
-
-
-
 int main(int argc, char* argv[]) {
-	game_t game; // not certain of world constructors etc
+	play();
+} 
+
+int play() {
+	game_t game;  // not certain of world constructors etc
 	game.running = 1;
 	while (game.running) {
 		// do something, run the game
 		update(&game);
-
 		usleep(16666);
-
 	}
 }
 
