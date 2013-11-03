3265d1cf8bfdbe2b0139bc5005010b299df99bb1
Victor Jiao <VjiaoBlack@gmail.com>
2013-11-02 21:59:34 -0400

    Updated menu.

--- terminvaders.c
+++ terminvaders.c
@@ -6,32 +6,81 @@
 
 /* Start the game by clearing the screen, etc. */
 void start(void) {
+    setbuf(stdout, NULL);
     xt_par0(XT_CLEAR_SCREEN);
 }
 
 /* Main menu: render the menu and return an option selected by the user. */
 int menu(void) {
     int key, choice = 0;
-    
+    int cursor_r = 1, cursor_c = 1;
+    xt_par2(XT_SET_ROW_COL_POS, 1, 1);
+    while (cursor_c <= COLS) {
+        cursor_r = 1;
+        xt_par2(XT_SET_ROW_COL_POS, cursor_r, cursor_c);
+        printf("-");
+        cursor_r = ROWS;
+        xt_par2(XT_SET_ROW_COL_POS, cursor_r, cursor_c);
+        printf("-");
+        cursor_c++;
+    }
+    cursor_r = 2;
+    while (cursor_r < ROWS) {
+        cursor_c = 1;
+        xt_par2(XT_SET_ROW_COL_POS, cursor_r, cursor_c);
+        printf("|");
+        cursor_c = COLS;
+        xt_par2(XT_SET_ROW_COL_POS, cursor_r, cursor_c);
+        printf("|");
+        cursor_r++;
+    }
+
     xt_par2(XT_SET_ROW_COL_POS, ROWS / 4, COLS / 2 - 11);
     printf("-~<[ terminvaders ]>~-");
 
-    xt_par2(XT_SET_ROW_COL_POS, 1, 1);
+    xt_par2(XT_SET_ROW_COL_POS, ROWS / 2, COLS / 2 - 2);
+    printf("Play");
 
+    xt_par2(XT_SET_ROW_COL_POS, 5 * ROWS / 8, COLS / 2 - 4);
+    printf("Settings");
 
-    printf(" Play\n Settings\n Quit\n");
+    xt_par2(XT_SET_ROW_COL_POS, 3 * ROWS / 4, COLS / 2 - 2);
+    printf("Quit");
+
     xt_par2(XT_SET_ROW_COL_POS, 1, 1);
     while (1) {
         switch ((key = getkey())) {
             case KEY_UP:
             case 'w':
                 choice = !choice ? 2 : choice - 1;
-                xt_par2(XT_SET_ROW_COL_POS, choice + 1, 1);
+                switch (choice) {
+                    case 0:
+                        xt_par2(XT_SET_ROW_COL_POS, ROWS / 2, COLS / 2 - 2);
+                        break;
+                    case 1:
+                        xt_par2(XT_SET_ROW_COL_POS, 5 * ROWS / 8, COLS / 2 - 4);
+                        break;
+                    case 2:
+                        xt_par2(XT_SET_ROW_COL_POS, 3 * ROWS / 4, COLS / 2 - 2);
+                        break;
+                }
+                printf("%d", choice);
                 break;
             case KEY_DOWN:
             case 's':
                 choice = (choice + 1) % 3;
-                xt_par2(XT_SET_ROW_COL_POS, choice + 1, 1);
+                switch (choice) {
+                    case 0:
+                        xt_par2(XT_SET_ROW_COL_POS, ROWS / 2, COLS / 2 - 2);
+                        break;
+                    case 1:
+                        xt_par2(XT_SET_ROW_COL_POS, 5 * ROWS / 8, COLS / 2 - 4);
+                        break;
+                    case 2:
+                        xt_par2(XT_SET_ROW_COL_POS, 3 * ROWS / 4, COLS / 2 - 2);
+                        break;
+                }
+                printf("%d", choice);
                 break;
             case KEY_ENTER:
                 return choice;
@@ -67,5 +116,6 @@ int main(int argc, char* argv[]) {
     start();
     loop();
     finish();
+    xt_par0(XT_CLEAR_SCREEN);
     return 0;
 }
