40b7cf626872c3c2c509c7af6420ae1325b3224a
Victor Jiao <VjiaoBlack@gmail.com>
2014-01-26 21:59:55 -0500

    starfield works; buffering bug. SETPOS() stops working suddenly

--- star.c
+++ star.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
-#include "xterm/keyboard.h"
-#include "xterm/xterm_control.h"
+#include <time.h>
+#include "xterm/keyboard.c"
+#include "xterm/xterm_control.c"
 
 #define FPS 60
-#define ROWS 40
-#define COLS 80
+#define ROWS 44
+#define COLS 160
 
 #define vo 10
 #define xo 50
+#define do 250
 #define theta 1.732
+#define numstars 100
+
+#define CLRSCRN() xt_par0(XT_CLEAR_SCREEN);
+#define SETPOS(x, y) xt_par2(XT_SET_ROW_COL_POS, (x), (y))
 
 struct star_t {
     int x;
@@ -20,6 +26,7 @@ struct star_t {
     int ywait;
     int roy;
     int rox;
+    int d;
 };
 typedef struct star_t star_t;
 
@@ -28,23 +35,32 @@ typedef struct star_t star_t;
 void update(star_t*);
 void display(star_t*);
 int handle_input(int);
-int calculate_wait(int, int, int);
+int calculate_wait(int, int);
+void dispframe();
 
 int main(int argc, char* argv[]) {
+    setbuf(stdout, NULL); //turns off buffering
+
     int running = 1;
 
-    star_t* stars = malloc(sizeof(star_t) * 100);
-    for (int i = 0; i < 100; ++i) {
-        int x = rand() % 20;
-        int y = rand() % 10;
+    dispframe();
 
-        int rox = rand() % (COLS / 3) + 1;
-        int roy = rand() % (ROWS / 3) + 1;
+    star_t* stars = malloc(sizeof(star_t) * numstars);
+    for (int i = 0; i < numstars; ++i) {
+        int rox = 0;
+        int roy = 0;
+        while (!rox) 
+            rox = rand() % 150 - 75;
+        while (!roy) 
+            roy = rand() % 150 - 75;
 
-        int xwait = calculate_wait(x, 1, rox);
-        int ywait = calculate_wait(y, 1, roy);
+        int x = ( (rox * xo) / (do + xo) ) + COLS / 2; //( (rox * sqrt(3)) / (do + xo)) * COLS + (COLS / 2);
+        int y = ( (roy * xo) / (do + xo) ) + ROWS / 2; //( (roy * sqrt(3)) / (do + xo)) * ROWS + (ROWS / 2);
 
-        stars[i] = (star_t) {x, y, xwait, ywait, rox, roy};
+        int xwait = calculate_wait(1, rox);
+        int ywait = calculate_wait(0, roy);
+
+        stars[i] = (star_t) {x, y, xwait, ywait, rox, roy, do};
     }
 
     while(running) {
@@ -57,26 +73,89 @@ int main(int argc, char* argv[]) {
 
 
 void update(star_t* stars) {
-    for (int i = 0; i < 100; ++i) {
+
+    for (int i = 0; i < numstars; ++i) {
+
         if (stars[i].xwait <= 0) {
-            stars[i].xwait = calculate_wait(stars[i].x, 1, stars[i].rox);
+            stars[i].xwait = calculate_wait(1, abs(stars[i].x - COLS / 2));
+            if (stars[i].x > COLS / 2)
+                stars[i].x = stars[i].x + 1;
+            else
+                stars[i].x = stars[i].x - 1;
+
         } else {
             stars[i].xwait--;
         }
         if (stars[i].ywait <= 0) {
-            stars[i].ywait = calculate_wait(stars[i].y, 1, stars[i].roy);
+            stars[i].ywait = calculate_wait(0, abs(stars[i].y - ROWS / 2));
+
+            if (stars[i].y > ROWS / 2)
+                stars[i].y = stars[i].y + 1;
+            else
+                stars[i].y = stars[i].y - 1;
+            
+
         } else {
             stars[i].ywait--;
         }
 
+        if (stars[i].x >= COLS || stars[i].x <= 0 || stars[i].y >= ROWS || stars[i].y <= 0){
+            int rox = 0;
+            int roy = 0;
+            while (!rox) 
+                rox = rand() % 150 - 75;
+            while (!roy) 
+                roy = rand() % 150 - 75;
+
+            int x = ( (rox * xo) / (do + xo) ) + COLS / 2; //( (rox * sqrt(3)) / (do + xo)) * COLS + (COLS / 2);
+            int y = ( (roy * xo) / (do + xo) ) + ROWS / 2; //( (roy * sqrt(3)) / (do + xo)) * ROWS + (ROWS / 2);
+
+            int xwait = calculate_wait(1, rox);
+            int ywait = calculate_wait(0, roy);
+
+            stars[i] = (star_t) {x, y, xwait, ywait, rox, roy, do};
+        }
 
     }
 }
 
 void display(star_t* stars) {
-    for (int i = 0; i < 100; ++i) {
-        xt_par2(XT_SET_ROW_COL_POS, stars[i].x, stars[i].y);
-        printf(".\n");
+    CLRSCRN();
+
+    for (int i = 0; i < numstars; ++i) {
+        SETPOS(stars[i].y, stars[i].x);
+        if (abs(stars[i].x - COLS / 2) + abs(stars[i].y - ROWS / 2) > 30) 
+            putchar('*');
+        else 
+            putchar('.');
+    }
+    dispframe();
+}
+
+void dispframe(){
+    
+    int cursor_r = 1, cursor_c = 1;
+    SETPOS(1, 1);
+    while (cursor_c <= COLS) {
+        cursor_r = 1;
+        SETPOS(cursor_r, cursor_c);
+        putchar('-');
+        cursor_r = ROWS;
+        SETPOS(cursor_r, cursor_c);
+        putchar('-');
+        cursor_c++;
+        usleep(1);
+    }
+    cursor_r = 2;
+    while (cursor_r <= ROWS) {
+        cursor_c = 1;
+        SETPOS(cursor_r, cursor_c);
+        putchar('|');
+        cursor_c = COLS;
+        SETPOS(cursor_r, cursor_c);
+        putchar('|');
+        cursor_r++;
+        usleep(1);
     }
 }
 
@@ -92,13 +171,13 @@ int handle_input(int running) {
     return running;
 }
 
-int calculate_wait(int pos, int is_horiz, int r) {
+int calculate_wait(int is_horiz, int r) {
     int w;
     if (is_horiz)
         w = COLS;
     else 
         w = ROWS;
 
-    int f = ( ((pos + xo)*(pos + xo)) / (vo * r * w * theta) );
-    return f;
+    int f = ( ((do + xo)*(do + xo)*(do + xo)) / (vo * r * r * w * theta) ) / 20;
+    return f;   
 }
