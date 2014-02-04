c58d91de53912bae8c5dc3ac55b552c1b94dee84
Ben Kurtovic <ben.kurtovic@gmail.com>
2014-02-04 01:03:20 -0500

    Testing.

--- lobby.c
+++ lobby.c
@@ -1,5 +1,8 @@
 #include "lobby.h"
 #include "server.h"
+#include "network.h"
+#include "transmit.h"
+#include "client.h"
 
 //ben, this should be coming from your server instead of these variables
 static multiplayergame_t* games;
@@ -50,10 +53,10 @@ int lobby() {
     games[0] = (multiplayergame_t) {1,"join this room if you have swag", 10, 4, 2, users1};//lobby_t current_lobby = (lobby_t) {"join this room if you have swag", 10, 4, 2, users1, NULL, NULL};
     games[1] = (multiplayergame_t) {1,"join this room if you dont have swag :(", 4, 1, 1, users2};//current_lobby.next = &((lobby_t) {"join this room if you dont have swag :(", 4, 1, 1, users2, NULL, &current_lobby});
     games[2] = (multiplayergame_t) {1,"pls dont join this room ty", 2, 1, 0, users3};
-    for (int i = 3; i < 10; i++) {
+    for (i = 3; i < 10; i++) {
         games[i] = (multiplayergame_t){1,"testtesttest", i, 4, 0, users1};
     }
-    for (int i = 10; i < MAX_GAMES; i++) {
+    for (i = 10; i < MAX_GAMES; i++) {
         games[i] = (multiplayergame_t) {0};
     }
     numgames = 9;
@@ -222,7 +225,8 @@ int game() {
     xt_par0(XT_CH_NORMAL);
     SETPOS(2, COLS - 18);
     printf("Who's Online");
-    for (int i = 0; i < numusers - onlinelistoffset; i++) {
+    int i;
+    for (i = 0; i < numusers - onlinelistoffset; i++) {
         SETPOS(4 + i, COLS - 18);
         if (5 + i > ROWS)
             break;
@@ -236,7 +240,7 @@ int game() {
         xt_par0(XT_CH_NORMAL);
         SETPOS(2, COLS - 18);
         printf("Who's Online");
-        for (int i = 0; i < numusers - onlinelistoffset; i++) {
+        for (i = 0; i < numusers - onlinelistoffset; i++) {
             SETPOS(4 + i, COLS - 18);
             if (5 + i > ROWS)
                 break;
@@ -253,7 +257,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -269,7 +273,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -293,7 +297,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -311,7 +315,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -326,7 +330,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -341,7 +345,7 @@ int game() {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -621,7 +625,7 @@ void game_wait(int created_game) {
     xt_par0(XT_CH_NORMAL);
     SETPOS(2, COLS - 18);
     printf("Who's Online");
-    for (int i = 0; i < numusers - onlinelistoffset; i++) {
+    for (i = 0; i < numusers - onlinelistoffset; i++) {
         SETPOS(4 + i, COLS - 18);
         if (5 + i > ROWS)
             break;
@@ -662,7 +666,7 @@ void game_wait(int created_game) {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -680,7 +684,7 @@ void game_wait(int created_game) {
                 xt_par0(XT_CH_NORMAL);
                 SETPOS(2, COLS - 18);
                 printf("Who's Online");
-                for (int i = 0; i < numusers - onlinelistoffset; i++) {
+                for (i = 0; i < numusers - onlinelistoffset; i++) {
                     SETPOS(4 + i, COLS - 18);
                     if (5 + i > ROWS)
                         break;
@@ -702,10 +706,10 @@ void serverlogin(){
 
     int field = 0, pos = 0, len = 0, addresslen = 0, usernamelen = 0, key = 0;
 
-    char* address = malloc(sizeof(char) * 33);
+    char address[33];
     int i = strlen(address);
 
-    char* username = malloc(sizeof(char) * 17);
+    char username[17];
     int j = strlen(username);
 
     while (i < 32) {
@@ -783,16 +787,71 @@ void serverlogin(){
 
         while((key = getkey()) == KEY_NOTHING);
         switch(key){
-            case KEY_ENTER:
+            case KEY_ENTER: {
                 // ben this shud be ur server magic,  you gotta connect here
                 // NOTE -> the NULL should be replaced with the user that calls this... i'm not sure how to best do it
-                free(address);
-                lobby();
+
+
+
+                // int sockfd = make_connection("earwig.yulli.org"), command;
+                // char* databuf;
+                // transmit(sockfd, CMD_CONNECT, "Earwig");
+                // transmit(sockfd, CMD_SETUP_GAME, "0|2|Test");
+                // while((key = getkey()) == KEY_NOTHING);
+                // receive(sockfd, &command, &databuf); // PLAYER_JOIN
+                // // free(databuf);
+                // receive(sockfd, &command, &databuf); // NEW_REQUEST
+                // // free(databuf);
+                // transmit(sockfd, CMD_ACCEPT_REQ, "1");
+                // receive(sockfd, &command, &databuf); // PLAYER_JOIN
+                // // free(databuf);
+                // receive(sockfd, &command, &databuf); // GAME_START
+                // // free(databuf);
+                // int flags = fcntl(sockfd, F_GETFL, 0);
+                // fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+                // game_t game;
+                // setup_game(&game);
+                // setup_multiplayer(&game, 2, 0, sockfd);
+                // while (game.running) {
+                //     update_game(&game);
+                //     usleep(1000000 / FPS);
+                // }
+
+
+
+
+                int sockfd = make_connection("earwig.yulli.org"), command;
+                char* databuf;
+                transmit(sockfd, CMD_CONNECT, "Firefly");
+                transmit(sockfd, CMD_JOIN_GAME, "0");
+                receive(sockfd, &command, &databuf); // PLAYER_JOIN
+                // free(databuf);
+                receive(sockfd, &command, &databuf); // PLAYER_JOIN
+                // free(databuf);
+                receive(sockfd, &command, &databuf); // GAME_START
+                // free(databuf);
+                int flags = fcntl(sockfd, F_GETFL, 0);
+                fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+                game_t game;
+                setup_game(&game);
+                setup_multiplayer(&game, 2, 1, sockfd);
+                while (game.running) {
+                    update_game(&game);
+                    usleep(1000000 / FPS);
+                }
+
+
+
+
+
+
+
+                // lobby();
                 //return 1;
                 return;
+            }
             case 27: //ESC
                 xt_par0(XT_CLEAR_SCREEN);
-                free(address);
                 //return 0;
                 return;
             case KEY_UP:
@@ -861,13 +920,6 @@ void serverlogin(){
                 break;
         }
     }
-
-
-
-
-
-    //you should connect to the server hereeeee
-    lobby();
 }
 
 int confirm_request(user_t requester) {
